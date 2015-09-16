de3bf4693e73e5d617448f9400c006955de3e126
crdlc <crdlc@tid.es>
2015-09-16 12:33:52 +0200

    added modal to remove videos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 
-var edit = document.querySelector('.video-toolbar');
+var videos = document.getElementById('videos');
 
-edit.addEventListener('click', function() {
-  var videos = document.getElementById('videos');
+var editToolbar = document.querySelector('.edit-toolbar');
+
+editToolbar.addEventListener('click', function() {
   videos.classList.toggle('edit');
+});
+
+var cancelButton = document.querySelector('.remove-toolbar .cancel');
+
+cancelButton.addEventListener('click', function cancelClicked() {
+  videos.classList.toggle('edit');
+});
+
+var removeButton = document.querySelector('.remove-toolbar .remove');
+
+removeButton.addEventListener('click', function() {
+  var modal = document.querySelector('.remove-modal');
+  modal.classList.toggle('show');
+  modal.addEventListener('click', function modalClicked() {
+    modal.removeEventListener('click', modalClicked);
+    modal.classList.toggle('show');
+  });
 });
